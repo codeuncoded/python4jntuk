20491ac0ea8cfe1138f12e9b6c5ad917520642ca
sreekanth <sreekanth.kolamala@gmail.com>
2017-06-15 11:18:59 +0530

    all basic lab programs added. Minor changes to textbook

--- Basics.py
+++ Basics.py
@@ -22,6 +22,12 @@ for i in range(2, 11):
 print result
 
 #3c what is sequence
+numbers = [2,3,4,5]
+for i in numbers:
+    print i
+numbers = (2,3,4,5)
+for i in numbers:
+    print i
 
 #3d
 number = int(input("enter any number:"))
@@ -52,6 +58,22 @@ def countWordFrequency(name):
             wordFrequency[char] = 1
     return wordFrequency
 print countWordFrequency(name)
+
+#5a alternate solution
+
+import collections
+def countFrequency(name):
+    wordFrequency = collections.defaultdict(int)
+    for char in name:
+        wordFrequency[char] += 1
+    return dict(wordFrequency)
+
+#5a Alternate solution
+
+import collections
+name = raw_input("enter a string")
+print dict(collections.Counter(name))
+
 #5b
 #don't know what trace is
 
@@ -92,19 +114,29 @@ while fileContent:  #remember an empty list [] evaluates to False
 
 
 #8a
+import math
+def ball_collide(ballA, ballB):
+    x1, y1, r1 = ballA
+    x2, y2, r2 = ballB
+    distance = math.sqrt((x2-x1)**2 + (y2-y1)**2)
+    return distance <= math.pi * (r1*r1 + r2*r2)
 
+#8a alternate
+import math
+def ball_collide((x1, y1, r1), (x2, y2, r2)):
+    distance = math.sqrt((x2-x1)**2 + (y2-y1)**2)
+    return distance <= math.pi * (r1*r1 + r2*r2)
 #8b
 
 def mean(numbers):
     return float(sum(numbers)) / max(len(numbers), 1)
-
 
 def median(numbers):
     if not numbers:
         return 0
     sortedNumbers = sorted(numbers)
     length = len(sortedNumbers)
-    return (sortedNumbers[length/2] + sortedNumbers[(length-1)/2])/2
+    return (sortedNumbers[length/2] + sortedNumbers[(length-1)/2])/2.0
 
 #if you are going to sort numbers, there is simpler way to find mode
 def mode(numbers):
@@ -138,10 +170,45 @@ print nearlyEqual("sree", "srek")
 print nearlyEqual("sree", "sek")
 print nearlyEqual("sree", "serk")
 
+#9b
 
+def dups(array):
+    frequencies = countWordFrequency(numbers)
+    duplicates = [key for key, value in frequencies.items() if value > 1]
+    return duplicates
 
+numbers = range(10) + range(5) + range(9,18)
+dups(numbers)
 
+#9c
+def unique(array):
+    duplicates = set(dups(array))
+    elements = set(array)
+    return elements.difference(duplicates)
 
+unique(numbers)
 
+#10a
+import operator
+def cumulative_product(numbers):
+    return reduce(operator.mul, numbers, 1)
 
+#10b
+def reverse(elements):
+    result = []
+    length = len(elements)
+    for i in range(length):
+        result.append(elements[length-1-i])
+    return result
+
+#10b alternate using list comprehensions
+
+def reverse(elements):
+    length = len(elements)
+    return [elements[length-1-i] for i in range(length)]
+
+#10b one-liner
+
+def reverse(elements):
+    return [elements[len(elements)-1-i] for i in range(len(elements))]
 
