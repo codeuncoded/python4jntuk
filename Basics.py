f95c0144c48a4311c22bd77f3a60621ef8ab90fe
sreekanth <sreekanth.kolamala@gmail.com>
2017-06-24 11:58:20 +0530

    more oop and exception handling added

--- Basics.py
+++ Basics.py
@@ -10,7 +10,7 @@ print distance
 #Exercise 2b
 from sys import argv
 script, first, second = argv
-print int(first) + int(second)
+print float(first) + float(second)
 
 #Exercise 3a
 
@@ -106,6 +106,7 @@ fileContent = file.readlines()
 fileContent.reverse()
 for line in fileContent:
     print line
+file.close()
 
 #7a other way
 filename = 'sample.py'  #create a file named 'sample.py' on your local drive
@@ -113,6 +114,15 @@ file = open(filename, 'r')
 fileContent = file.readlines()  #returns a list of strings: one string per line in the file
 while fileContent:  #remember an empty list [] evaluates to False
     print fileContent.pop() #pop() removes the last element in the list
+file.close()
+
+#7a another way
+with open('sample.py', 'r') as file:
+    fileContent = file.readlines()
+    while fileContent:
+        print fileContent.pop()
+
+
 
 #7b
 
@@ -222,6 +232,16 @@ def reverse(elements):
         result.append(elements[length-1-i])
     return result
 
+#10b an alternate solution
+def reverse(elements):
+    result = []
+    length = len(elements)
+    for _ in range(length):
+        result.append(elements.pop())
+    return result
+    #however, you are modifying the input here.
+
+
 #10b alternate using list comprehensions
 
 def reverse(elements):
@@ -233,3 +253,102 @@ def reverse(elements):
 def reverse(elements):
     return [elements[len(elements)-1-i] for i in range(len(elements))]
 
+def reverse(elements):
+    return reversed(elements)
+
+#11a broken
+def addMatrix(a,b):
+    rows = len(a)
+    columns = len(a[0])
+    result = [[0 for _ in range(columns)] for _ in range(rows)]
+    for row in range(rows):
+        for column in range(columns):
+            result[row][column] = a[row][column] + b[row][column]
+    return result
+
+#11a
+
+def addMatrix(a,b):
+    return [map(operator.add, rowA, rowB) for rowA, rowB in zip(a,b)]
+
+#11c numpy solution
+
+def tranpose(a):
+    rows = len(a)
+    columns = len(a[0])
+    # [a[row][column] for column in range(columns) for row in range(rows)]
+    result = [[0 for _ in range(rows)] for _ in range(columns)]
+    for row in range(rows):
+        for column in range(columns):
+            result[column][row] = a[row][column]
+    for column in range(columns):
+        for row in range(rows):
+            result[column][row] = a[row][column]
+
+def addNumber(a, b):
+    no_of_rows = len(a)
+    no_of_columns = len(a[0])
+    return [[a[i][j] + b for j in range(no_of_columns)] for i in range(no_of_rows)]
+
+def addNumber(a,b):
+    no_of_rows = len(a)
+    no_of_columns = len(a[0])
+    result = [[0 for i in range(no_of_columns)] for j in range(no_of_rows)]
+    for i in range(no_of_rows):
+        for j in range(no_of_columns):
+            result[i][j] = a[i][j] + b
+    return result
+
+def tranpose(a):
+    no_of_rows = len(a)
+    no_of_columns = len(a[0])
+    return [[a[i][j] for i in range(no_of_rows)] for j in range(no_of_columns)]
+
+def multiply_matrix(mat_a, mat_b):
+    a = len(mat_a)
+    b = len(mat_b[0])
+    result = [[0 for _ in range(b)] for _ in range(a)]
+    for i in range(a):
+        for j in range(b):
+            for k in range(len(mat_a[0])):
+                result[i][j] += mat_a[i][k]*mat_b[k][j]
+    return result
+
+def multiply_matrix(mat_a, mat_b):
+    trans_b = tranpose(mat_b)
+    return [[sum(map(operator.mul, mat_a[i], trans_b[j])) for j in range(len(trans_b))] for i in range(len(mat_a))]
+
+
+
+def mulMatrix(a,b):
+    import numpy
+    return numpy.matmul(a,b)
+
+#16a
+
+
+class Queue(object):
+    def __init__(self):
+        self.elements = []
+
+    def enqueue(self, elem):
+        self.elements.append(elem)
+
+    def deque(self):
+        return self.elements.pop(0)
+
+    def __len__(self):
+        return len(self.elements)
+
+    def __repr__(self):
+        return str(self.elements)
+
+queue = Queue()
+queue.enqueue(1)
+queue.enqueue(2)
+queue.enqueue(3)
+queue.enqueue(4)
+print queue
+print queue.deque()
+print queue
+print len(queue)
